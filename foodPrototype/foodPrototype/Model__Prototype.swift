32eeeb9a4d22c91f5645a20eea814927379263ee
송동욱 <quino0627@korea.ac.kr>
2018-07-27 20:00:34 +0900

    Model_Protype 파일 생성
    대희꺼 바탕으로 작성, 추후 비교 후 개선작업 필요

---
+++ foodPrototype/foodPrototype/Model__Prototype.swift
@@ -0,0 +1,73 @@
+//
+//  Model__Prototype.swift
+//  foodPrototype
+//
+//  Created by 송 on 2018. 7. 27..
+//  Copyright © 2018년 송. All rights reserved.
+//
+
+import Foundation
+
+typealias Time = (Year:Int, Month:Int, Day:Int, Hour:Int, Minute:Int)
+enum postClassify {
+    case groupBuying
+    case directDeal
+    case operatorPost
+}
+
+enum foodType{
+    case banchan
+    case fruit
+}
+
+struct comment{
+    var commentDate: Time
+    var commentWriter:user
+    var commentContent:String
+    var commentReplyArray = Array<comment>()
+}
+
+struct productInfo{
+    var productPicArray = Array<imageFile>()
+    var productType: foodType
+    var productExplanation: String
+    
+}
+
+struct post {
+    var postDate: Time
+    var postWriter: user
+    var postType : postClassify
+    var postTitle:String
+    var postContent:productInfo
+    var postLike: Int
+    var postCommentArray = Array<comment>()
+}
+
+struct imageFile {}
+
+struct user {
+    var userName:String
+    var userNickname:String
+    var userImage:imageFile
+    var userId:String
+    var userPassword:String
+    var schoolCertification:Bool
+    var sellHistory = Array<post>()
+    var buyHispory = Array<post>()
+    var myWritingHistory = Array<post>()
+    var myWishList = Array<post>()
+    var belongingChatRoomArray = Array<chatRoom>()
+    var userReliability:Int //Need Improvement
+}
+
+var groupBuyingPostArray = Array<post>()
+var directDealPostArray = Array<post>()
+var operatorPostArray = Array<post>()
+var chatRoomArray = Array<chatRoom>()
+
+struct chatRoom{
+    var chatLeader:user
+    var chatMember = Array<user>()
+    //채팅 데이터 추가
+}
