20c6e7075778fc5bf609f6f54ce33752647b4e60
송동욱 <quino0627@korea.ac.kr>
2018-08-07 17:59:17 +0900

    stepped progress bar 사용 가능

--- foodPrototype/foodPrototype/TabBar/StepAddPostViewController.swift
+++ foodPrototype/foodPrototype/TabBar/StepAddPostViewController.swift
@@ -1,49 +1,88 @@
 //
-//  StepAddPostViewController.swift
-//  foodPrototype
+//  ViewController.swift
+//  StepIndicator
 //
-//  Created by 송 on 2018. 8. 7..
-//  Copyright © 2018년 송. All rights reserved.
+//  Created by Yun Chen on 2017/7/14.
+//  Copyright © 2017 Yun CHEN. All rights reserved.
 //
 
 import UIKit
-import SnapKit
-import StepIndicator
 
-class StepAddPostViewController: UIViewController {
+class StepAddPostViewController: UIViewController,UIScrollViewDelegate {
     
-    let stepIndicatorView = StepIndicatorView()
-
+    @IBOutlet weak var stepIndicatorView:StepIndicatorView!
+    @IBOutlet weak var scrollView:UIScrollView!
+    
+    private var isScrollViewInitialized = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.stepIndicatorView.frame = CGRect(x: 0, y: 50, width: 280, height: 100)
-        self.view.addSubview(self.stepIndicatorView)
-        stepIndicatorView.snp.makeConstraints{(make) in
-            
-        }
         
-        self.stepIndicatorView.numberOfSteps = 3
-        self.stepIndicatorView.currentStep = 0
+        //In this demo, the customizations have been done in Storyboard.
         
-        
-        
+        //Customization by coding:
+        //self.stepIndicatorView.numberOfSteps = 5
+        //self.stepIndicatorView.currentStep = 0
+        //self.stepIndicatorView.circleColor = UIColor(red: 179.0/255.0, green: 189.0/255.0, blue: 194.0/255.0, alpha: 1.0)
+        //self.stepIndicatorView.circleTintColor = UIColor(red: 0.0/255.0, green: 180.0/255.0, blue: 124.0/255.0, alpha: 1.0)
+        //self.stepIndicatorView.circleStrokeWidth = 3.0
+        //self.stepIndicatorView.circleRadius = 10.0
+        //self.stepIndicatorView.lineColor = self.stepIndicatorView.circleColor
+        //self.stepIndicatorView.lineTintColor = self.stepIndicatorView.circleTintColor
+        //self.stepIndicatorView.lineMargin = 4.0
+        //self.stepIndicatorView.lineStrokeWidth = 2.0
+        //self.stepIndicatorView.displayNumbers = false //indicates if it displays numbers at the center instead of the core circle
+        //self.stepIndicatorView.direction = .leftToRight
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if !isScrollViewInitialized {
+            isScrollViewInitialized = true
+            self.initScrollView()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    private func initScrollView() {
+        self.scrollView.contentSize = CGSize(width: self.scrollView.frame.width * CGFloat(self.stepIndicatorView.numberOfSteps + 1), height: self.scrollView.frame.height)
+        for i in 1...self.stepIndicatorView.numberOfSteps + 1 {
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100))
+            
+            if i == 1 {
+                label.text = "Image Picker"
+                //함수화
+            }
+            else if i==2 {
+                label.text = "제목, 가격, 최대 인원수"
+            }
+            else if i==3{
+                label.text = "희망 구매 사이트, 회망 거래 위치, 추가 기술"
+            }
+            else{
+                label.text = "Done"
+            }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+            label.textAlignment = NSTextAlignment.center
+            label.font = UIFont.systemFont(ofSize: 35)
+            label.textColor = UIColor.lightGray
+            label.center = CGPoint(x: self.scrollView.frame.width / 2.0 * (CGFloat(i - 1) * 2.0 + 1.0), y: self.scrollView.frame.height / 2.0)
+            self.scrollView.addSubview(label)
+        }
     }
-    */
-
+    
+    
+    // MARK: - UIScrollViewDelegate
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageIndex = scrollView.contentOffset.x / scrollView.frame.size.width
+        stepIndicatorView.currentStep = Int(pageIndex)
+    }
+    
 }
+
+
