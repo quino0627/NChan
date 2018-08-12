2de242f6834406f7328db08d5684b85b64a6bcea
송동욱 <quino0627@korea.ac.kr>
2018-08-12 22:50:03 +0900

    이미지도 표시

--- foodPrototype/foodPrototype/TabBar/MyPageTableViewController.swift
+++ foodPrototype/foodPrototype/TabBar/MyPageTableViewController.swift
@@ -20,6 +20,19 @@ class MyPageTableViewController: UITableViewController {
         Database.database().reference().child("users").child(uid!).observeSingleEvent(of: DataEventType.value, with: {(datasnapshot) in
             let value = datasnapshot.value as! NSDictionary
             self.profileName.text = value["name"] as! String
+            let ssss = value["profileImageUrl"] as! String
+            print(ssss)
+            print("ㄴㄴㄴㄴ")
+            //let url = URL(string: value.allValues[0] as! String)
+            let url = URL(string: ssss as! String)
+            URLSession.shared.dataTask(with: url!, completionHandler: {(data, response, err) in
+                DispatchQueue.main.sync {
+                    self.profileImage.image = UIImage(data:data!)
+                    self.profileImage.layer.cornerRadius = self.profileImage.frame.width/2
+                    self.profileImage.layer.masksToBounds = true
+                }
+            }).resume()
+            
         })        // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
